aa0a379ccdc57efde0397149e3864458b16716c9
Trangnguyenthiquynh <124223179+Trangnguyenthiquynh@users.noreply.github.com>
2024-05-06 15:21:45 +0700

    fix

--- week3/bt1-3/validateuser.php
+++ week3/bt1-3/validateuser.php
@@ -1,23 +1,20 @@
 <?php
-session_start(); // Khởi tạo phiên làm việc
+session_start(); 
 ob_start();
-// Kiểm tra xem có dữ liệu được gửi từ biểu mẫu POST hay không
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Kết nối đến cơ sở dữ liệu
     $servername = "localhost";
-    $username = "root"; // Thay thế username bằng tên người dùng của bạn
-    $password = ""; // Thay thế password bằng mật khẩu của bạn
-    $dbname = "login"; // Thay thế myDB bằng tên cơ sở dữ liệu của bạn
+    $username = "root"; 
+    $password = ""; 
+    $dbname = "login"; 
 
-    // Tạo kết nối
     $conn = new mysqli($servername, $username, $password, $dbname);
 
-    // Kiểm tra kết nối
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
 
-    // Lấy dữ liệu từ biểu mẫu
     $username = $_POST['username'];
     $password = $_POST['password'];
 
@@ -25,13 +22,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = mysqli_real_escape_string($conn, $username);
     $password = mysqli_real_escape_string($conn, $password);
 
-    // Tạo truy vấn SQL để kiểm tra xem người dùng có tồn tại không
     $sql = "SELECT * FROM users WHERE username = '$username' AND pass = '$password'";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
         // Đăng nhập thành công
-        $_SESSION["isLogin"] = true; // Thiết lập biến Session cho trạng thái đăng nhập thành công
+        $_SESSION["isLogin"] = true; 
         header("Location: Logout.php"); 
         exit(); // Kết thúc kịch bản PHP
     } else {
