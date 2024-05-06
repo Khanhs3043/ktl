e59452a6b7685faa8ed155f157c5510fde2ebc0c
XuanKhanh3043 <124223086+Khanhs3043@users.noreply.github.com>
2024-05-06 09:27:34 +0000

    update w3

--- week3/b1/validateuser.php
+++ week3/b1/validateuser.php
@@ -1,5 +1,17 @@
 <?php 
     session_start();
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "accountmana";
+    
+    $conn = new mysqli($servername, $username, $password, $database);
+    
+    if ($conn->connect_error) {
+        die("Kết nối đến cơ sở dữ liệu thất bại: " . $conn->connect_error);
+    }
+    $sql = "SELECT username, password FROM accounts";
+    $result = $conn->query($sql);
     if (!isset($_SESSION['isLogin'])) {
         $_SESSION['isLogin'] = false;
     }
@@ -7,14 +19,18 @@
         header('Location: login.html');
     }
     else header('Location: logout.php');
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
-        if($_POST['username']=='khanh' && $_POST['password']=='123456')
-        {   
-            $_SESSION['isLogin'] = true;
-            header('Location: logout.php');
-            exit;
-        } else {
-            echo "Thông tin đăng nhập không hợp lệ?";
-        }
+    if($_SERVER["REQUEST_METHOD"] == "POST"){ 
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                if($_POST['username']==$row["username"] && $_POST['password']==$row["password"])
+                {   
+                    $_SESSION['isLogin'] = true;
+                    header('Location: logout.php');
+                    exit;
+                } else {
+                    echo "Thông tin đăng nhập không hợp lệ?";
+                }
+            } }    
+        
     }
 ?>
