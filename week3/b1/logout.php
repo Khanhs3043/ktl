5029e79303a1311c05968273d239669865feeb67
XuanKhanh3043 <124223086+Khanhs3043@users.noreply.github.com>
2024-05-06 01:26:54 +0000

    fix validateUser

--- week3/b1/logout.php
+++ week3/b1/logout.php
@@ -8,3 +8,20 @@ if($_SERVER["REQUEST_METHOD"]== "POST"){
     header("Location: login.html");
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Homepage</title>
+</head>
+<body>
+    <div class="con">
+        <h1 class="noti">Đăng nhập thành công !</h1>
+        <form action="logout.php" method="post">
+            <button class="logoutBtn" type="submit">Log out</button>
+        </form>
+    </div>
+</body>
+</html>
--- week3/b1/validateuser.php
+++ week3/b1/validateuser.php
@@ -1,34 +1,20 @@
 <?php 
     session_start();
-    $_SESSION['isLogin'] == false;
-    $noti = "";
-    if($_SESSION['isLogin'] == false) 
+    if (!isset($_SESSION['isLogin'])) {
+        $_SESSION['isLogin'] = false;
+    }
+    if($_SESSION['isLogin'] == false) {
         header('Location: login.html');
+    }
+    else header('Location: logout.php');
     if($_SERVER["REQUEST_METHOD"] == "POST"){
-        if($_POST['username']=='khanh' && $_POST['password']==='123456')
+        if($_POST['username']=='khanh' && $_POST['password']=='123456')
         {   
             $_SESSION['isLogin'] = true;
-            $noti = "Đăng nhập thành công";
+            header('Location: logout.php');
+            exit;
         } else {
-            header('Location: login.html');
+            echo "Thông tin đăng nhập không hợp lệ?";
         }
     }
-    
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Homepage</title>
-</head>
-<body>
-    <div class="con">
-        <h1 class="noti"><?php echo $noti?> !</h1>
-        <form action="logout.php" method="post">
-            <button class="logoutBtn" type="submit">Log out</button>
-        </form>
-    </div>
-</body>
-</html>
