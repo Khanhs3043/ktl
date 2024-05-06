5ddbd4dc410e57623ac993c2ecf12bf45c47ff87
XuanKhanh3043 <124223086+Khanhs3043@users.noreply.github.com>
2024-05-06 15:25:45 +0700

    fix w2 b2

--- week3/b2/index.php
+++ week3/b2/index.php
@@ -1,86 +1,75 @@
 <?php
-    $files = scandir("upload/");
-    $noti = "";   
-    $dir = "upload/"; 
-    $sortBySize = true;
-    $buttonName = "Sort by size";
-    $fileList = $files;
-                function compareBySize($a, $b) {
-                    return filesize("upload/$a") - filesize("upload/$b");
-                }
-                           
-                usort($fileList, 'compareBySize');
-                if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                    if (isset($_POST["sort"])) {
-                        $sortBySize = !$sortBySize;
+    if(isset($_FILES['file'])) {
+        $uploadDir = "upload/";
+        $uploadFile = $uploadDir . basename($_FILES["file"]["name"]);
+        
+        if(move_uploaded_file($_FILES["file"]["tmp_name"], $uploadFile)) {
+            echo "<p class = 'noti'>Tải tệp thành công!</p>";
+        } else {
+            echo "<p class = 'noti'>Vui lòng chọn tệp để tải lên!</p>";
+        }
+    }
 
-                    }
-                }
+    //thông tin chi tiết các tệp
+    $uploadDir = "upload/";
+    $files = scandir($uploadDir);
+
+    $sortType = isset($_GET['sort']) ? $_GET['sort'] : 'name';
+    $sortOrder = isset($_GET['order']) ? $_GET['order'] : 'asc';
+
+    if($sortType == 'name') {
+        if($sortOrder == 'asc') {
+            natcasesort($files); // tăng dần
+        } else {
+            natcasesort($files);
+            $files = array_reverse($files); // giảm dần
+        }
+    } elseif($sortType == 'date') {
+        if($sortOrder == 'asc') {
+            usort($files, function($a, $b) use ($uploadDir) {
+                return filemtime($uploadDir . $a) - filemtime($uploadDir . $b); // tăng dần
+            });
+        } else {
+            usort($files, function($a, $b) use ($uploadDir) {
+                return filemtime($uploadDir . $b) - filemtime($uploadDir . $a); // giảm dần
+            });
+        }
+    }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Upload file</title>
+    <title>File Upload</title>
 </head>
 <body>
-    <form action="index.php" class="upload" method="post" enctype="multipart/form-data">
-        <input class="upIn" type="file" name="myfile">
-        <button class="upBtn" >Browse</button> 
+    <h2 class="title">Upload File</h2>
+    <form action="index.php" method="post" enctype="multipart/form-data" class = "upload">
+        <input type="file" name="file">
+        <button type="submit" name="submit">Upload</button>
     </form>
-    <?php
-        if(isset($_FILES['myfile']['name']) && $_FILES['myfile']['name'] != "") {
-            $fileupload = $dir . $_FILES['myfile']['name']; 
-            if(move_uploaded_file($_FILES['myfile']['tmp_name'], $fileupload)) {
-                $noti = "Upload file thành công!";
-            } else {
-                $noti = "Upload file không thành công!";
-            }
-        } else {
-            $noti = "Vui lòng chọn file để upload!";
-        }
-    ?>
-    <div class="sp">
-        <span><?php echo $noti?>     <br> 
-        <form action="index.php" class="sortType" method="post">
-        <button type="submit" class="sort" name="sort">
-            <?php echo $sortBySize ? 'sortBySize' : 'sortByName'; ?>
-        </button>
-    
-        </form>
-    </span></div>
-        <table class="tbl" border="1">
-            <tr>
-                <th>No</th>
-                <th>Filename</th>
-                <th>Type</th>
-                <th>Upload time</th>
-                <th>Size (bytes)</th>
-            </tr> 
-            <?php 
-                
-                $no = 0;
-                foreach ($fileList as $file) {
-                    
-                    if ($file != '.' && $file != '..') {
-                        $no ++;
-                        $fileName = $file;
-                        $fileType = mime_content_type("upload/$file");
-                        $fileSize = filesize("upload/$file");
-                        $uploadDate = date("Y-m-d H:i:s", filemtime("upload/$file"));
-                        echo "<tr>
-                        <td>$no</td>
-                        <td>$fileName</td>
-                        <td>$fileType</td>
-                        <td>$uploadDate</td>
-                        <td>$fileSize </td>
-                    </tr> ";
-                    }
-                }
-            ?>
-        </table>
-   
+
+    <h2 class="title">Uploaded Files</h2>
+    <table class="tbl" border =1>
+        <tr>
+            <th><a href="index.php?sort=name&order=<?php echo ($sortType == 'name' && $sortOrder == 'asc') ? 'desc' : 'asc'; ?>">Tên tệp</a></th>
+            <th>Loại tệp</th>
+            <th><a href="index.php?sort=date&order=<?php echo ($sortType == 'date' && $sortOrder == 'asc') ? 'desc' : 'asc'; ?>">Ngày tải lên</a></th>
+            <th>Kích thước</th>
+        </tr>
+        <?php foreach ($files as $file) : ?>
+            <?php if ($file != '.' && $file != '..') : ?>
+                <tr>
+                    <td><?php echo $file; ?></td>
+                    <td><?php echo mime_content_type("upload/$file"); ?> bytes</td>
+                    <td><?php echo date("Y-m-d H:i:s", filemtime($uploadDir . $file)); ?></td>
+                    <td><?php echo filesize($uploadDir . $file); ?> bytes</td>
+                </tr>
+            <?php endif; ?>
+        <?php endforeach; ?>
+    </table>
 </body>
 </html>
