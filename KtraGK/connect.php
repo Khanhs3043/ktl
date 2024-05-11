f82974ca5254af57085c1cc569273240925008ac
Trangnguyenthiquynh <124223179+Trangnguyenthiquynh@users.noreply.github.com>
2024-05-11 14:38:35 +0000

    fixGK

---
+++ KtraGK/connect.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+require '/index.php';
+
+if (!isset($_SESSION['isLogin'])) {
+    $_SESSION['isLogin'] = false;
+}
+if($_SESSION['isLogin'] == false) {
+    header('Location: ../View/Login.html');
+}else header('Location: sach.php');
+
+if($_SERVER["REQUEST_METHOD"] == "POST") { 
+    $username = $_POST['username'];
+    $pass = $_POST['password'];
+    $result = $db->query("SELECT * FROM User where tenUser = '$username' and MatKhau = '$pass'");
+    if($result->rowCount() >= 1) {
+                $_SESSION['isLogin'] = true;
+                header('Location: ../Controller/sach.php');
+                exit;
+            } else {
+                $_SESSION['isLogin'] = false;
+            }   
+    }
+?>
